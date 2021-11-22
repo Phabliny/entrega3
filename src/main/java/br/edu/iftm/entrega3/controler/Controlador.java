3272c952856a8c269ec368149b19fb8543afe9a5
Phabliny <phablinymartins@gmail.com>
2021-11-22 18:24:44 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/iftm/entrega3/controler/Controlador.java
@@ -0,0 +1,41 @@
+package br.edu.iftm.entrega3.controler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.iftm.entrega3.model.Usuario;
+import br.edu.iftm.entrega3.repository.UsuarioRepository;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Controlador {
+    @Autowired 
+    UsuarioRepository repo;
+
+    @RequestMapping("/usuarios")
+    String inicio(Model modelo){
+        List<Usuario> usuarios = repo.buscaTodosUsuarios();
+        modelo.addAttribute("usuarios", usuarios);
+        return "usuarios";
+    }
+
+    @GetMapping("/form-usuario")
+    String formUsuario(Model modelo){
+        modelo.addAttribute("usuario", new Usuario());
+        return "form-usuario";
+    }
+
+    @PostMapping("/form-usuario")
+    public String cadastroUsuario(Usuario usuario) {
+        //TODO: process POST request
+        repo.gravaUsuario(usuario);
+        return "redirect:/usuarios";
+    }
+    
+}
---
+++ src/main/java/br/edu/iftm/entrega3/repository/UsuarioRepository.java
@@ -0,0 +1,44 @@
+package br.edu.iftm.entrega3.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import br.edu.iftm.entrega3.model.Usuario;
+
+public class UsuarioRepository {
+    
+    @Autowired
+    JdbcTemplate jdbc;
+
+    public List<Usuario> buscaTodosUsuarios(){
+        String consulta = "select * from usuario;";
+        return jdbc.query(consulta,
+                (resultados, numeroDaLinha) -> new Usuario(resultados.getInt("id"), resultados.getString("nome"), resultados.getString("cpf"), resultados.getString("email")));
+    }
+
+    public int gravaUsuario(Usuario usuario){
+        String consulta = "insert into usuario(nome, cpf, email) values(?,?,?)";
+        return jdbc.update(consulta, usuario.getNome(), usuario.getCpf(), usuario.getEmail());
+    }
+
+    public int excluirUsuario(Integer id){
+        System.out.println(" -------------> id = " + id);
+        String consulta = "delete from usuario where id = ?";
+        return jdbc.update(consulta, id);
+    }
+
+    public Usuario buscaPorId(Integer id) {
+        return jdbc.queryForObject(
+            "select * from usuario where id = ?",
+            (resultSet, rowNum) -> { return new Usuario(resultSet.getInt("id"),resultSet.getString("nome"), resultSet.getString("cpf"), resultSet.getString("email")); },
+            id);
+    }
+
+    public int atualizaUsuario(Usuario usuario){
+        String consulta = "update usuario set nome = ? where id = ?";
+        return jdbc.update(consulta, usuario.getId(), usuario.getNome(), usuario.getCpf(), usuario.getEmail());
+    }
+
+}
